32e5d650bbfbbedfda09d4aaa7efbe3316369875
WenruiLi <24947837+WenruiLi0203@users.noreply.github.com>
2018-02-24 16:39:18 -0800

    Upload
    Upload

---
+++ Client.java
@@ -0,0 +1,182 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+public class Client {
+	private Socket socketClient;
+	private PrintWriter out;
+	private BufferedReader stdIn;
+	private volatile AtomicBoolean isFinished = new AtomicBoolean();
+	public String linein=null;
+	private boolean auth=false;
+	Scanner sysIn = new Scanner(System.in);
+	static ArrayList <String> ignore = new ArrayList<String>();
+	
+	public static void main(String argv[]) throws Exception
+    {
+		   Client test = new Client();
+		   addignore();//Server messages ignored words.
+		   test.connectServer();
+		   test.options();//Client menu.
+		   System.exit(0);
+
+    }
+	
+	//connect to the server
+	public void connectServer() throws Exception {
+		stdIn =new BufferedReader(new InputStreamReader(System.in));
+		/*System.out.println("Please enter the server hostname:");
+		String hostName = stdIn.readLine();
+		System.out.println("Please enter the server port number:");
+		String portString = stdIn.readLine();
+		int port = Integer.parseInt(portString);
+		*/
+		try {
+			//socketClient = new Socket("cyberlab.pacific.edu",12000);
+			//socketClient = new Socket(hostName,port);
+			socketClient = new Socket("localhost",8006);
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Invalid hostname/port number.");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// send client message thread.
+		final Thread outThread = new Thread() {
+			@Override
+			public void run() {
+				//System.out.println("Connected to: "+hostName+", "+port);
+				try {
+					out = new PrintWriter(socketClient.getOutputStream(),true);
+					out.println("HELLO");//initialize connection.
+					out.flush();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			};
+		};
+		outThread.start();
+		
+		//receive server message thread.
+		final Thread inThread = new Thread() {
+			@Override
+			public void run() {
+				// Use a Scanner to read from the remote server
+				Scanner in = null;
+				
+				try {
+					in = new Scanner(socketClient.getInputStream());
+					linein = in.nextLine();
+					while (!isFinished.get()) {
+						// change/ignore received server message printouts.
+						if(linein.contains("SIGNIN")) {
+							System.out.println(linein.substring(7, linein.length())+" signed in.");
+						}else if(linein.contains("SIGNOFF")) {
+							System.out.println(linein.substring(8, linein.length())+" signed off.");
+						}else if(linein.contains("FROM")) {
+							System.out.println("Message from "+ linein.substring(5, linein.length()));
+						}
+						//ignore
+						else if(!ignore.contains(linein)) {
+							System.out.println(linein);
+						}
+						//get next line.
+						linein = in.nextLine();
+						//check login boolean.
+						if(linein.equals("AUTHYES")) {
+							auth = true;
+						}
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			};
+		};
+		inThread.start();
+		Thread.sleep(300);//delay for receiving server messages.
+		login();
+		//out.println("AUTH:wli7:p398");
+
+	}
+	
+	public void login() throws InterruptedException {
+		String LoginName,Password;
+	    while(!auth) {
+		    System.out.println("Please enter a username:");
+		    LoginName = sysIn.nextLine();
+		    System.out.println("Please enter a password:");
+		    Password=sysIn.nextLine();
+		    out.println("AUTH:"+LoginName+":"+Password);
+		    Thread.sleep(300);//delay for receiving server messages.
+		    if(!auth) {
+		    	System.out.println("Incorrect username and/or password.");
+		    }
+	    }
+	    System.out.println("You are now authenticated.");
+	}
+	
+	//client menu
+	public void options() throws IOException, InterruptedException {
+		String command = null;
+		String sUser,sMsg;
+		int clientoptions = 0;
+
+    	while(clientoptions!=3) {// 3 = sign off.
+
+    		Thread.sleep(300);//delay for server input.
+			optionsmenu();//print out 3 options.
+	    	command=stdIn.readLine();
+
+	    	if((!command.equals("1"))&&(!command.equals("2"))&&(!command.equals("3"))) {//input check
+	    		System.out.println("Invaid input.");
+	    	}else {
+	    		clientoptions=Integer.parseInt(command);
+	    		
+		    	//LIST
+		    	if(clientoptions==1) {
+		    		out.println("LIST");
+		    		System.out.println("Users currently logged in:");
+		    	}
+		    	//SEND MSG
+		    	else if(clientoptions==2) {
+		    		System.out.println("User you would like to message:");
+		    		sUser=stdIn.readLine();
+		    		System.out.println("Message:");
+		    		sMsg=stdIn.readLine();
+		    		out.println("TO:"+sUser+":"+sMsg);
+		    		System.out.println("Message sent.");
+		    	}
+		    	//SIGN OFF
+		    	else if(clientoptions==3) {
+		    		System.out.println("Signing off...");
+		    		out.println("BYE");
+		    	    socketClient.close();
+		    	}
+	    	}
+    	}
+	}
+	
+	//client menu printout
+	public void optionsmenu() {
+    	System.out.println("Choose an option:");
+    	System.out.println("1. List online users");
+    	System.out.println("2. Send someone a message");
+    	System.out.println("3. Sign off\r");
+	}
+	
+	//ignored terms that should not print out.
+	public static void addignore() {
+	   ignore.add("HELLO");
+	   ignore.add("AUTHYES");
+	   ignore.add("AUTHNO");
+	}
+}
+
---
+++ SocketServer.java
@@ -0,0 +1,217 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class SocketServer extends Thread {
+    public static final int PORT_NUMBER = 8005;
+    
+    static ServerSocket server = null;
+    protected static Socket socket = null;
+    
+    InputStream in = null;
+    OutputStream out = null;
+    
+    public String getuser=null;
+    static SocketServer client;
+    
+    static ArrayList <String> usernameAndpassword = new ArrayList<String>();
+    final static List<SocketServer> clients = new ArrayList<>();
+    
+    //create server socket and start
+    private SocketServer(Socket socket) {
+        this.socket = socket;
+        System.out.println("New client connected from " + socket.getInetAddress().getHostAddress());
+        start();
+    }
+    
+    //main
+    public static void main(String[] args) throws IOException {
+        System.out.println("SocketServer Example");
+        test_user_and_password(); //add test user and password in the server.
+        try {
+            server = new ServerSocket(PORT_NUMBER);
+            while (true) {
+                client = new SocketServer(server.accept());
+                //System.out.println(client.getuser+"\r\n");
+                //System.out.println(client.getName()+"\r\n");
+            }
+        } catch (IOException ex) {
+            System.out.println("Unable to start server.");
+        } finally {
+            try {
+                if (server != null)
+                    server.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    //run loop
+    public void run() {
+        try {
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String request;//client input
+            
+            //HELLO method part
+            request = br.readLine();
+            inital(request);
+            
+            //AUTH method part
+            request = br.readLine();
+            boolean authyes = false;
+            authyes = login(request);
+            while(!authyes) {
+            	request = br.readLine();
+            	authyes = login(request);
+            }
+            
+            //SIGNED IN
+            request = br.readLine();
+            while(!request.equals("BYE")) {
+            	if(request.equals("LIST")) {//LIST method part
+            		listuser();
+            		request = br.readLine();
+            	}else if(request.substring(0,3).equals("TO:")) {//SEND message part
+            		sendmessage(request);
+            		request = br.readLine();
+            	}else {
+            		break;
+            	}
+            }
+            out.write("BYE\r\n".getBytes());
+ 
+        } catch (IOException ex) {
+            System.out.println("Unable to get streams from client");
+        } finally {
+            try {
+            	clients.remove(client);
+    			for(SocketServer client : clients) {
+    				client.out.write(("SIGNOFF: "+getuser+"\r\n").getBytes());
+    			}
+                in.close();
+                out.close();
+                socket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    
+    //add list of usernames and passwords.
+	public static void test_user_and_password() {
+		usernameAndpassword.add("test1.p000");
+		usernameAndpassword.add("test2.p000");
+		usernameAndpassword.add("test3.p000");
+		usernameAndpassword.add("test4.p000");
+		usernameAndpassword.add("wli7.p398");
+	}
+	
+	//HELLO method 
+    public void inital(String request) throws IOException {
+    	if(request.equals("HELLO")) {
+    		out.write("HELLO\r\n".getBytes());
+    	}else {
+    		out.write("Connection closed by foreign host.\r\n".getBytes());
+    		server.close();
+    	}
+    }
+
+    //AUTH method
+    public boolean login(String request) throws IOException {
+    	String[] parts = null;
+    	String getpw=null;
+    	String authpart = null;
+    	try {
+    		parts = request.split("\\:");//Separate client input to three parts by ":"
+    		authpart = parts[0];
+    		getuser = parts[1];
+    		getpw = parts[2];
+    		
+        	if(authpart.equals("AUTH")) {
+        		boolean auth = authentication(getuser, getpw);//check username and password
+        		if(auth) {
+        			out.write("AUTHYES\r\n".getBytes());
+        			client.setName(getuser);
+        			clients.add(client);
+        			//System.out.println(client.getName()+"\r\n");
+        			for(SocketServer client : clients) {//print out to all clients that someone sign in.
+        				client.out.write(("SIGNIN: "+getuser+"\r\n").getBytes());
+        			}
+        			//System.out.println("SIGNIN:"+getuser);
+        			return true;
+        		}else {
+        			out.write("AUTHNO\r\n".getBytes());
+        			return false;
+        		}
+        	}
+    	}catch(Exception e){
+    		out.write("Connection closed by foreign host.\r\n".getBytes());
+    		server.close();
+
+        }
+    	return false;
+    }
+    
+    //check username and password exist
+    public boolean authentication(String username, String password) {
+    	String combin = username+"."+password;
+    	for(String string : usernameAndpassword) {
+    		if(string.equals(combin)) {
+    			return true;
+    		}
+    	}
+		return false;
+    }
+    
+    //LIST method
+    public void listuser() throws IOException {
+    	for(SocketServer client : clients) {
+    		out.write(client.getName().getBytes());
+    		out.write(",".getBytes());
+    	}
+    	out.write("\r\n".getBytes());
+    	//System.out.println(clients.toString());
+    }
+    
+    //Send message method
+    public void sendmessage(String request) throws IOException {
+    	String[] parts = null;
+    	String user=null;
+    	String message = null;
+    	int index = -1;
+    	try {
+    		parts = request.split("\\:");//separate client input to three parts by ":"
+    		user = parts[1];
+    		message = parts[2];
+    		index = findUserIndex(user);//find the clients list index.
+    		//System.out.println(index);
+    		//System.out.println(clients.get(index));
+    		if(index!=-1) {
+    			clients.get(index).out.write(("FROM:"+getuser+":"+message+"\r\n").getBytes());//print out the message at receiver.
+    		}
+    	}catch(Exception e){
+    		out.write("Send Message Error.\r\n".getBytes());
+        }
+    }
+    
+    //find client list index by username
+    public int findUserIndex(String username) {
+    	for(SocketServer client : clients) {
+    		if(client.getName().equals(username)) {
+    			return clients.indexOf(client);
+    		}
+    	}
+		return -1;
+    }
+
+}
